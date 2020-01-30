ce1274d422247106b5ff05c7f0f4989c409f9362
ochui princewill patrick <ochuiprincewill411@gmail.com>
2020-01-30 17:22:51 +0100

    added Ad model to django admin

--- property_ads/admin.py
+++ property_ads/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    Category
+    Category, Ad, AdImage
 )
 
 @admin.register(Category)
@@ -8,3 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
     
     list_display = ['name', 'parent']
     list_filter = ['name', 'parent']
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    
+    list_display = ['title', 'category', 'agent', 'region', 'place']
+    list_display = ['category', 'region', 'place']
